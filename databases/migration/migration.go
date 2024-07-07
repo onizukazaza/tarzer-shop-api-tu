98a1ddff4f592f888b3399b11999479a5983f3da
onizukazaza <onigame.dd@outlook.co.th>
2024-07-07 15:08:39 +0700

    :ocean: transection

--- databases/migration/migration.go
+++ databases/migration/migration.go
@@ -1,14 +1,53 @@
 package main
 
 import (
-	"fmt"
+	
 	"github.com/onizukazaza/tarzer-shop-api-tu/config"
 	"github.com/onizukazaza/tarzer-shop-api-tu/databases"
+	"github.com/onizukazaza/tarzer-shop-api-tu/entities"
+	"gorm.io/gorm"
 )
 
 func main() {
 	conf := config.ConfigGetting()
 	db := databases.NewPostgresDatabase(conf.Database)
 
-	fmt.Println(db.ConnectionGetting())
+	// fmt.Println(db.ConnectionGetting())
+	tx := db.ConnectionGetting().Begin()
+
+	playerMigration(tx)
+	adminMigration(tx)
+	itemMigration(tx)
+	playerCoinMigration(tx)
+	inventoryMigration(tx)
+	purchaseHistoryMigration(tx)
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		panic(err)
+	}
+}
+
+func playerMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Player{})
+}
+
+func adminMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Admin{})
+}
+
+func itemMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Item{})
+}
+
+func playerCoinMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Playercoin{})
+}
+
+func inventoryMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Inventory{})
+}
+
+func purchaseHistoryMigration(tx *gorm.DB) {
+	tx.Migrator().CreateTable(&entities.Purchasehistory{})
 }
