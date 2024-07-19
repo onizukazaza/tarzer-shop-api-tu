4ab06a833eb084de76780338d0542d6ddc344bce
onizukazaza <onigame.dd@outlook.co.th>
2024-07-19 21:47:48 +0700

    :sparkle: fix file

--- pkg/itemManaging/repository/itemManagingRepository.go
+++ pkg/itemManaging/repository/itemManagingRepository.go
@@ -1,18 +1,3 @@
 package repository
 
-import (
-	"github.com/labstack/echo/v4"
-	"gorm.io/gorm"
-)
-
-type itemManagingRepositoryImpl struct {
-	db     *gorm.DB
-	logger echo.Logger
-}
-
-func NewItemManagingRepositoryImpl(db *gorm.DB, logger echo.Logger) *itemManagingRepositoryImpl {
-	return &itemManagingRepositoryImpl{
-		db:     db,
-		logger: logger,
-	}
-}
+type ItemManagingRepository interface {}
--- pkg/itemManaging/repository/itemManagingRepositoryImpl.go
+++ pkg/itemManaging/repository/itemManagingRepositoryImpl.go
@@ -1,3 +1,18 @@
 package repository
 
-type ItemManagingRepository interface {}
+import (
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+type itemManagingRepositoryImpl struct {
+	db     *gorm.DB
+	logger echo.Logger
+}
+
+func NewItemManagingRepositoryImpl(db *gorm.DB, logger echo.Logger) *itemManagingRepositoryImpl {
+	return &itemManagingRepositoryImpl{
+		db:     db,
+		logger: logger,
+	}
+}
